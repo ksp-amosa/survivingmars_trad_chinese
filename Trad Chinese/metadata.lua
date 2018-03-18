b871f1cae7f44313962c7927eaa684419d32977c
twitch.tv/amosa2001 <ksp.amosa@gmail.com>
2018-03-18 19:55:50 +0800

    V22

--- Trad Chinese/metadata.lua
+++ Trad Chinese/metadata.lua
@@ -1,12 +1,12 @@
 return PlaceObj('ModDef', {
 	'title', "繁體中文模組 Traditional Chinese",
-	'description', "繁體中文模組由阿MOSA主持，社群提供翻譯內容。\n因文本內容高達五萬字以上的內容，八千筆以上的條目，目前已可以維持遊戲性的翻譯內容優先。\n\n翻譯進度：70%\n github.com/ksp-amosa/survivingmars_trad_chinese \n感謝阿MOSA實況社群的默默協助 :D\n 轉載 重製 編輯內容請先來信詢問 ksp.amosa@gmail.com",
+	'description', "繁體中文模組由阿MOSA主持，社群提供翻譯內容。目前翻譯進度僅剩下不影響遊戲的部分以及幾則謎團故事的翻譯，控制器相關的內容翻譯會較不完整，建議使用鍵盤與滑鼠體驗 \n\n翻譯進度：85%\n github.com/ksp-amosa/survivingmars_trad_chinese \n感謝阿MOSA實況社群的默默協助 :D\n 轉載 重製 編輯內容請先來信詢問 ksp.amosa@gmail.com",
 	'tags', "Localization",
 	'image', "SteamPreviewImage.png",
 	'id', "JtYVpy",
 	'steam_id', "1331777861",
 	'author', "twitch.tv/amosa2001",
-	'version', 21,
+	'version', 22,
 	'lua_revision', 227923,
 	'loctables', {
 		{
@@ -14,5 +14,5 @@ return PlaceObj('ModDef', {
 			language = "Any",
 		},
 	},
-	'saved', 1521297067,
+	'saved', 1521374032,
 })
